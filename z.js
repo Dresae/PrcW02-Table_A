3fc4218364f1e8f99b08cbb81c08c047fbed0ce7
lvdev <lov3r.dev@gmail.com>
2024-11-19 22:44:28 +0000

    toggle js function added

---
+++ z.js
@@ -0,0 +1,6 @@
+function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+    const icon = document.querySelector('.theme-toggle i');
+    icon.classList.toggle('fa-moon');
+    icon.classList.toggle('fa-sun');
+}
